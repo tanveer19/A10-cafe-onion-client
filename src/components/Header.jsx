e7bbcc69114f67e6bac7c7dd5a0e118f0f056db8
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-05-03 16:50:59 +0600

    started creating route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,40 +4,19 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <nav className="navbar navbar-expand-lg bg-light">
-              <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
-                  Cafe Tanveer
-                </Link>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavAltMarkup"
-                  aria-controls="navbarNavAltMarkup"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarNavAltMarkup"
-                >
-                  <div className="navbar-nav">
-                    <Link to="/Home">Home</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/login">Login</Link>
-                  </div>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
+    <div className="header fixed-top d-flex align-items-center">
+      <div className="container d-flex align-items-center justify-content-between">
+        <Link to="/" className="logo d-flex align-items-center me-auto me-lg-0">
+          <h1>
+            Cafe Tanveer<span>.</span>
+          </h1>
+        </Link>
+
+        <nav id="navbar" className="navbar">
+          <Link to="/Home">Home</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/login">Login</Link>
+        </nav>
       </div>
     </div>
   );
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAtdA_F3dfZHaWza4ylaGb4j7kFIrsedE",
+  authDomain: "a10-chef-recipe-hunter-8182f.firebaseapp.com",
+  projectId: "a10-chef-recipe-hunter-8182f",
+  storageBucket: "a10-chef-recipe-hunter-8182f.appspot.com",
+  messagingSenderId: "418694586922",
+  appId: "1:418694586922:web:9c3c61c478f246869b1953",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
