9dced577b3368dd5dfb0c2f6dc115e6023e6e8f6
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-05-04 10:23:07 +0600

    added footer

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,27 +20,8 @@ const Login = () => {
       });
   };
   return (
-    <div className="form-container">
-      <h2 className="form-title">Login</h2>
-      <form>
-        <div className="form-control">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="" required />
-        </div>
-        <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="" required />
-        </div>
-        <input className="btn-submit" type="submit" value="Login" />
-      </form>
-      <p>
-        <small>
-          New to Ema-john? <Link to="/signup">Create New Account</Link>
-        </small>
-      </p>
-      <div>
-        <button onClick={handleGoogleSignIn}>Google Login</button>
-      </div>
+    <div>
+      <h2>Login</h2>
     </div>
   );
 };
