b59a7cce745a94cedc85043d18ec856e753ca6d4
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-03 21:26:16 +0600

    showing chef data on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [chefs, setChefs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/chefs")
+      .then((res) => res.json())
+      .then((data) => setChefs(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
+      {/* banner start */}
+
       <div
         className="hero h-96"
         style={{ backgroundImage: "url(/img/banner.jpg)" }}
@@ -13,6 +24,18 @@ const Home = () => {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Cafe Onion</h1>
           </div>
+        </div>
+      </div>
+
+      {/* chef section */}
+      <div>
+        <h4> All Chefs</h4>
+        <div>
+          {chefs.map((chef) => (
+            <p key={chef.id}>
+              <Link to={`/chef/${chef.id}`}>{chef.Name}</Link>
+            </p>
+          ))}
         </div>
       </div>
     </div>
