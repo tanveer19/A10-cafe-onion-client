ecc86907fd926909bdeab9b9ae4da45359dd42b7
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-09 16:52:58 +0600

    changed menu slider type

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -62,25 +62,51 @@ const Home = () => {
       {/* menu item */}
       <h4 className="text-5xl text-center my-3"> Menu Item</h4>
 
-      <div>
-        <div className="h-96 carousel carousel-vertical rounded-box grid justify-center ">
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-1.png" />
+      <div className="w-1/2 mx-auto">
+        <div className="carousel ">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img src="/img/menu/menu-item-1.png" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-2.png" />
+          <div id="slide2" className="carousel-item relative w-full">
+            <img src="/img/menu/menu-item-2.png" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-3.png" />
+          <div id="slide3" className="carousel-item relative w-full">
+            <img src="/img/menu/menu-item-3.png" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-4.png" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-5.png" />
-          </div>
-          <div className="carousel-item h-full">
-            <img src="/img/menu/menu-item-6.png" />
+          <div id="slide4" className="carousel-item relative w-full">
+            <img src="/img/menu/menu-item-4.png" className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
         </div>
       </div>
