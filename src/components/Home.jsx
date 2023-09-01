aa679b8d3da60473b5538d63236aa5d6acc1aed9
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-09-02 00:00:01 +0600

    chef bg change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Chef from "./Chef";
+import Gallery from "./Gallery";
 
 const Home = () => {
   const [chefs, setChefs] = useState([]);
@@ -29,7 +30,7 @@ const Home = () => {
       </div>
 
       {/* chef section */}
-      <div>
+      <div className="bg-[#eeeeee]">
         <h4 className="text-5xl text-center my-3"> All Chefs</h4>
         <div className="grid grid-cols-2 md:grid-cols-3 md:w-3/4 mx-auto p-2">
           {chefs.map((chef) => (
@@ -40,24 +41,7 @@ const Home = () => {
 
       {/* gallery */}
 
-      <div className="">
-        <h4 className="text-5xl text-center my-3"> Gallery</h4>
-
-        <div className="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
-          <div className="carousel-item">
-            <img src="/img/gallery/gallery-1.jpg" className="rounded-box" />
-          </div>
-          <div className="carousel-item">
-            <img src="/img/gallery/gallery-2.jpg" className="rounded-box" />
-          </div>
-          <div className="carousel-item">
-            <img src="/img/gallery/gallery-3.jpg" className="rounded-box" />
-          </div>
-          <div className="carousel-item">
-            <img src="/img/gallery/gallery-4.jpg" className="rounded-box" />
-          </div>
-        </div>
-      </div>
+      <Gallery></Gallery>
 
       {/* menu item */}
       <h4 className="text-5xl text-center my-3"> Menu Item</h4>
